09e009f305c7bfadb0b27b1cd37527bfe1cff794
manolo <manolo@idealis.es>
2017-07-26 22:43:08 +0200

    borrada version beta primera maquetacion del dashboard

--- lib-js/sqlite.js
+++
@@ -1,1 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
--- lib-js/tests/general-test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe("TRANTOR Test Suite", function() {
-  describe('TODO\n', function() {
-    it('', function() {
-      assert.equal(a, b);
-    });
-  });
-})
