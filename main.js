9ce7522dee82b4a72accfbe3c01c97f3eea18ca5
Andersson <ander7agar@gmail.com>
2017-10-05 22:36:21 +0200

    added drag-drop added morre translations added slide functionalities

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function createWindow () {
     win.maximize();
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'slide-1.html'),
         protocol: 'file:',
         slashes: true
     }));
