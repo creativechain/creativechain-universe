0f8ca4b6560e037804d08290e49a996dfc241da7
ander7agar <ander7agar@gmail.com>
2018-01-22 21:14:42 +0100

    multiple changes

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function createWindow () {
     }));
 
     // Open the DevTools.
-    //platformWindow.webContents.openDevTools();
+    platformWindow.webContents.openDevTools();
 
     ticker();
     // Emitted when the window is closed.
